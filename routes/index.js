657da118c37849eafe4babc84e376373dd2393ea
rayme11 <vivachihuahua2004@gmail.com>
2015-11-18 15:38:04 -0500

    Change welcome lable.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'This is a test, to Express' });
+  res.render('index', { title: 'This is a test, to Express and azure' });
 });
 
 module.exports = router;
